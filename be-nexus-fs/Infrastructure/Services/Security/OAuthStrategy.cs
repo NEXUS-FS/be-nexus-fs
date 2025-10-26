8a42036c9dccf18e4c1de5c978d642d3bc869528
Aldea Andrei <aldea.andrei.977@gmail.com>
2025-10-26 14:53:16 +0200

    NEXUS-70: Security: Add OAuthStrategy skeleton

--- be-nexus-fs/Infrastructure/Services/Security/OAuthStrategy.cs
+++ be-nexus-fs/Infrastructure/Services/Security/OAuthStrategy.cs
@@ -6,5 +6,26 @@ namespace Infrastructure.Services.Security
 {
     public class OAuthStrategy : IAuthStrategy
     {
+        public string StrategyName => "OAuth2";
+
+        public Task<bool> AuthenticateAsync(Dictionary<string, string> credentials)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> GenerateTokenAsync(Dictionary<string, string> credentials)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RevokeTokenAsync(string token)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> ValidateTokenAsync(string token)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
