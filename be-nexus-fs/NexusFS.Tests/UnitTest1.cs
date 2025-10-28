6f1f082c51ab32bcab418628e8a865c852db73ac
Doltu Teodora - Eliza <66266646+elizadoltu@users.noreply.github.com>
2025-10-28 14:31:58 +0200

    fixed: test references

--- be-nexus-fs/NexusFS.Tests/UnitTest1.cs
+++ be-nexus-fs/NexusFS.Tests/UnitTest1.cs
@@ -3,8 +3,12 @@
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public void Test_ShouldPass()
     {
-
+        // Arrange
+        var result = 1 + 1;
+        
+        // Act & Assert
+        Assert.Equal(2, result);
     }
 }
